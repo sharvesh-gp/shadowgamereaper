7bcab59d3a4660c5717ec91fb3ac71002f8cd0e0
sharvesh-gp <sharvesh.gp76@gmail.com>
2025-07-12 22:51:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
+import { amplifyConfig } from './amplify-config';
 import Game from './Game';
+
+// Initialize Amplify
+Amplify.configure(amplifyConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Game />);
